297bda59da94ec3371ea1d1c53bed06bb1c69249
Qudsia Ahmad <q4hmad@gmail.com>
2017-12-29 20:26:11 -0800

    fix create, add flash and error messages, fix routing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  def index
+  end
   def new
     @article = Article.new
   end
@@ -8,7 +10,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
-      redirect_to article_path(:article)
+      redirect_to article_path(@article)
     else
       render 'new'
   end
